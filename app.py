d0313867a6cc6bedc4488fac3255f238663e134e
Дмитрий Игуменшев <ddmauushy@gmail.com>
2025-09-26 20:42:25 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
+
+@app.route("/lab1")
+def lab1():
+    return """<!doctype html>
+        <html>
+            <body>
+                <ul>
+                    <h1>Лабораторная работа 1</h1>
+                    <li><a href="/author">author</a></li>
+                    <li><a href="/web">web</a></li>
+                    <li><a href="/image">image</a></li>
+                </ul>
+            </body>
+        </html>"""    
+
 @app.route("/web")
 def web():
     return """<!doctype html>
@@ -27,3 +42,16 @@ def author():
                 <a href="/web">web</a>
             </body>
         </html>"""
+
+@app.route('/image') 
+def image():
+    path = url_for("static", filename="ocean.jpg")
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Океан</h1>
+        <img src="''' + path + '''">
+    </body>
+</html>
+'''
