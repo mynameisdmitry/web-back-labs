ee959cc103136c73a7185bb303feee4d920000d0
Дмитрий Игуменшев <ddmauushy@gmail.com>
2025-11-07 23:11:34 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -71,3 +71,43 @@ def pay():
 def success():
     price = request.args.get('price', 0)
     return render_template('lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    background_color = request.args.get('background_color')
+    font_size = request.args.get('font_size')
+    font_family = request.args.get('font_family')
+    
+    if color or background_color or font_size or font_family:
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if background_color:
+            resp.set_cookie('background_color', background_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        if font_family:
+            resp.set_cookie('font_family', font_family)
+        return resp
+    
+    color = request.cookies.get('color', '#000000')
+    background_color = request.cookies.get('background_color', '#414344')
+    font_size = request.cookies.get('font_size', '14')
+    font_family = request.cookies.get('font_family', 'Arial, sans-serif')
+    
+    return render_template('lab3/settings.html', 
+                         color=color,
+                         background_color=background_color,
+                         font_size=font_size,
+                         font_family=font_family)
+
+@lab3.route('/lab3/clear_settings')
+def clear_settings():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('background_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_family')
+    return resp
