513ac57b54231c11ab0d86e53049f84025ee88ba
Дмитрий Игуменшев <ddmauushy@gmail.com>
2025-12-19 22:20:34 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,27 +1,45 @@
-from flask import Blueprint, render_template
-import os
-
+from flask import Blueprint, render_template, request, redirect, session
 lab5 = Blueprint('lab5', __name__)
+import psycopg2
 
 @lab5.route('/')
-def main():
-    print("Рендерим шаблон lab5.html")
-    print("Путь к шаблону:", os.path.join('templates', 'lab5', 'lab5.html'))
-    print("Файл существует:", os.path.exists(os.path.join('templates', 'lab5', 'lab5.html')))
-    return render_template('lab5/lab5.html', username='anonymous')
 
-@lab5.route('/login')
-def login():
-    return "Страница входа"
-
-@lab5.route('/register')
 def register():
-    return "Страница регистрации"
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
 
-@lab5.route('/list')
-def list_articles():
-    return "Список статей"
+    if not (login and password):
+        return render_template('lab5/register.html', error='Заполните все поля')
+    
+    try:
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='dmitry_igumenshev_knowledge_base',
+            user='dmitry_igumenshev_knowledge_base',
+            password='Dima2005',
+            port=5432
+        )
+        cur = conn.cursor()
 
-@lab5.route('/create')
-def create_article():
-    return "Создание статьи"
+        cur.execute("SELECT login FROM users WHERE login = %s", (login,))
+        if cur.fetchone():
+            cur.close()
+            conn.close()
+            return render_template('lab5/register.html',
+                                error="Такой пользователь уже существует")
+        
+        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s)", (login, password))
+        conn.commit()
+        
+        cur.close()
+        conn.close()
+        
+        return render_template('lab5/success.html', login=login)
+    
+    except psycopg2.OperationalError as e:
+        return render_template('lab5/register.html', error=f'Ошибка подключения к БД: {str(e)}')
+    except Exception as e:
+        return render_template('lab5/register.html', error=f'Ошибка базы данных: {str(e)}')
